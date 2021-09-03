69a9995216f6295f7aab54cad812101e159a83b6
eldegen <eliyaz2761@gmail.com>
2021-09-03 12:46:05 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] nums = {4, -2.3, 7.4, -0.5, 10, 16.4, -9.1, 18.2, 0.2, 3.4, -14.2, 28, 99.9, -0.12, 3.12};
+        double averageNum = 0;
+        boolean hasNegative = false;
+
+        for (double tempVar : nums) {
+            if (tempVar <= 0 && !hasNegative) {
+                hasNegative = true;
+            }
+
+            if (tempVar > 0 && hasNegative) {
+                averageNum = averageNum + tempVar;
+                System.out.println("sssss " + averageNum);
+            }
+        }
+
+    }
+}
